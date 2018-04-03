cf0327a20585a2d37ddde034f444dba7582c17bd
Cara <cararbrennan@gmail.com>
2018-04-02 17:05:17 -0700

    Created Trips MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
+  get 'trips/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+    # Routes that operate on the trip collection
+    get '/trips', to: 'trips#index', as: 'trips'
+    get '/trips/new', to: 'trips#new', as: 'new_trip'
+    post '/trips', to: 'trips#create'
+
+    # Routes that operate on individual trips
+    get '/trips/:id', to: 'trips#show', as: 'trip'
+    get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+    patch '/trips/:id', to: 'trips#update'
+    delete '/trips/:id', to: 'trips#destroy'
+
 end
