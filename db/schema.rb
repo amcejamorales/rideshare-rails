464351224b543527078bd59dc853093aeb774b10
Cara <cararbrennan@gmail.com>
2018-04-03 12:25:06 -0700

    Initial Passenger MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402233853) do
+ActiveRecord::Schema.define(version: 20180403191340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phonenumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.integer "driver_id"
