c5f05bdb24f083f152e8b28295deacc5e03d533b
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-04 12:14:49 -0700

    Add #total_earnings to Driver and update show view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,16 @@
+include ActionView::Helpers::NumberHelper
+
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings
+    subtotal_cents = 0
+    self.trips.each do |trip|
+      subtotal_cents += ( trip.cost - 165 )
+    end
+    subtotal = subtotal_cents / 100
+    total = subtotal * 0.80
+    number_to_currency(total)
+  end
+  
 end
