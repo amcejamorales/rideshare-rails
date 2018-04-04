5e1b303b0255f9dd3d44422ec99a4114d47bb2ad
Cara <cararbrennan@gmail.com>
2018-04-04 15:52:28 -0700

    Added show method for trip and trips.

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-    resources :trips, except: [:new, :create]
+    resources :trips, except: [:new]
     resources :passengers do
       resources :trips, only: [:create]
-    end 
+    end
     resources :drivers
 
 end
