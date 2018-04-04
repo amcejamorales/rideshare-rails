ace6d112a2e6c4a586b8c99ffb41b6783225a468
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-04 12:19:56 -0700

    Add #average_rating to Driver and update view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -12,5 +12,15 @@ class Driver < ApplicationRecord
     total = subtotal * 0.80
     number_to_currency(total)
   end
-  
+
+  def average_rating
+    total_ratings = 0.0
+    self.trips.each do |trip|
+      total_ratings += trip.rating
+    end
+    number_of_trips = self.trips.length
+    average_rating = total_ratings / number_of_trips
+    number_with_precision(average_rating, precision: 1)
+  end
+
 end
