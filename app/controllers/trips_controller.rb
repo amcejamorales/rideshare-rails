4701cdc18958b45024afd45c63386a4875758ef4
Cara <cararbrennan@gmail.com>
2018-04-04 12:42:17 -0700

    Adding in Trips methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,21 +4,31 @@ class TripsController < ApplicationController
   end
 
   # def new
+  #   @trip Trip.new
   # end
 
   def create
+    @trip = Trip.new
   end
 
   def show
+
   end
 
   # def edit
   # end
 
   def update
+
   end
 
   def destroy
+
+  end
+
+  private
+  def trip_params
+    return params.require(:trip).permit(:passenger_id, :driver_id, :rating)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-    resources :trips
-    resources :passengers
+    resources :trips except: [:new, :create]
+    resources :passengers do
+      resources :trips, only: [:create]
     resources :drivers
 
 end
