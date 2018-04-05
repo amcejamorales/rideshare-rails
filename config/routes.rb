bf69b41f7213144c09afb6287ba932fe6e9eb600
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-05 13:32:33 -0700

    Set up navigation menu and home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'trips/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+    root 'home#index'
 
     resources :trips, except: [:new]
     resources :passengers do
