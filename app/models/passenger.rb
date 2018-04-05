2b2fb1b7c348f924edaad65935e45f188642a69d
Cara <cararbrennan@gmail.com>
2018-04-05 14:52:41 -0700

    Completed Passenger methods and links to trips, converted cents to dollars in Passenger and Trips views.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,14 @@
+include ActionView::Helpers::NumberHelper
+
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def passenger_total
+   total_cost = 0
+   self.trips.each do |trip |
+     total_cost += trip.cost
+   end
+   total_cost = total_cost/100
+   return number_to_currency(total_cost)
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,11 @@
+include ActionView::Helpers::NumberHelper
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+
+def convert_to_currency(n)
+  n= n/100
+  return number_to_currency(n)
+end
 end
